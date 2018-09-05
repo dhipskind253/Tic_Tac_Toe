ea3eb35c196b32421a18fd66d4d265bdd01388d2
Daniel Hipskind <dhipskind253@gmail.com>
2018-09-05 17:00:48 -0400

    First commit just changed a segmented change link

--- Tic Tac Toe/ViewController.swift
+++ Tic Tac Toe/ViewController.swift
@@ -9,10 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var numPlayer: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
